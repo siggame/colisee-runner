8cc7cb422854200aa913e8d7c11e0058acfa7c2f
user404d <conduffql@gmail.com>
2017-09-20 16:45:18 -0500

    Add helpers documentation

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,10 +3,21 @@ import * as _ from "lodash";
 
 interface IRetryOptions { attempts: number; timeout: number; }
 
+/**
+ * Retry fn using attempts as the maximum number of attempts using
+ * timeout as the timeout between attempts. Throws an error when
+ * maximum attempts have been reached.
+ *
+ * @export
+ * @param {IRetryOptions} { attempts, timeout }
+ * @param {(...args: any[]) => Promise<any>} fn
+ * @param {...any[]} args
+ * @returns {Promise<void>}
+ */
 export async function retry(
     { attempts, timeout }: IRetryOptions,
     fn: (...args: any[]) => Promise<any>,
-    ...args: any[]) {
+    ...args: any[]): Promise<void> {
     for (let i = 0; i < attempts; i++) {
         const success = await fn(...args).then(() => true).catch(() => false);
         if (success) {
@@ -18,35 +29,92 @@ export async function retry(
     throw new Error(`Max attempts reached for request. (${attempts})`);
 }
 
+/**
+ * Delay execution for ms milliseconds.
+ *
+ * @export
+ * @param {number} ms
+ */
 export async function delay(ms: number) {
     await new Promise((res, rej) => setTimeout(res, ms));
 }
 
+/**
+ * Helper for type problems on callbacks.
+ *
+ * @export
+ * @template T
+ * @returns {(first: T) => T}
+ */
 export function identity<T>(): (first: T) => T {
     return (first: T) => first;
 }
 
+/**
+ * Assert that value is not undefined or null.
+ *
+ * @export
+ * @template T
+ * @param {(T | undefined | null)} value
+ * @returns {value is T}
+ */
 export function not_nil<T>(value: T | undefined | null): value is T {
     return !_.isNil(value);
 }
 
+/**
+ * Create an infinite stream.
+ *
+ * @export
+ * @template T
+ * @returns {AsyncIterableIterator<T>}
+ */
 export async function* infinite<T>(): AsyncIterableIterator<T> {
     while (true) { yield; }
 }
 
-export async function* generate<T>(fn: (...args: any[]) => Promise<T>, ...args: any[]) {
+/**
+ * Create a stream of values that are the result of calling `fn` with `...args`.
+ *
+ * @export
+ * @template T
+ * @param {(...args: any[]) => Promise<T>} fn
+ * @param {...any[]} args
+ * @returns {AsyncIterableIterator<T>}
+ */
+export async function* generate<T>(fn: (...args: any[]) => Promise<T>, ...args: any[]): AsyncIterableIterator<T> {
     while (true) {
         yield await fn(...args);
     }
 }
 
-export async function* take<T>(n: number, iter: AsyncIterableIterator<T>) {
+/**
+ * Take n values generated from iter.
+ *
+ * @export
+ * @template T
+ * @param {number} n
+ * @param {AsyncIterableIterator<T>} iter
+ * @returns {AsyncIterableIterator<T>}
+ */
+export async function* take<T>(n: number, iter: AsyncIterableIterator<T>): AsyncIterableIterator<T> {
     for (let i = 0; i < n; i++) {
-        yield await iter.next();
+        yield (await iter.next()).value;
     }
     return;
 }
 
+/**
+ * Transform a stream of T into a stream of U.
+ *
+ * @export
+ * @template T
+ * @template U
+ * @param {AsyncIterableIterator<T>} iter
+ * @param {(value: T) => Promise<U>} async_callback
+ * @param {(error: any, value: T) => Promise<any>} error
+ * @returns {AsyncIterableIterator<U>}
+ */
 export async function* map<T, U>(
     iter: AsyncIterableIterator<T>,
     async_callback: (value: T) => Promise<U>,
@@ -58,6 +126,17 @@ export async function* map<T, U>(
     }
 }
 
+/**
+ * Asynchronously call async_callback on values generated
+ * by iter and yield incoming values.
+ *
+ * @export
+ * @template T
+ * @param {AsyncIterableIterator<T>} iter
+ * @param {(value: T) => Promise<T>} async_callback
+ * @param {(error: any, value: T) => Promise<any>} error
+ * @returns {AsyncIterableIterator<T>}
+ */
 export async function* async_foreach<T>(
     iter: AsyncIterableIterator<T>,
     async_callback: (value: T) => Promise<T>,
@@ -82,6 +161,16 @@ export async function* foreach<T>(
     }
 }
 
+/**
+ * Filter out values that do not satisfy the condition.
+ *
+ * @export
+ * @template T
+ * @template U
+ * @param {AsyncIterableIterator<T>} iter
+ * @param {(value: T) => value is U} condition
+ * @returns {AsyncIterableIterator<U>}
+ */
 export async function* filter<T, U extends T>(
     iter: AsyncIterableIterator<T>,
     condition: (value: T) => value is U,
@@ -103,6 +192,17 @@ export function coroutine<T, U, V extends IterableIterator<U> | AsyncIterableIte
     };
 }
 
+/**
+ * Send the values generated from iter to sink so that the values
+ * are consumed.
+ *
+ * @export
+ * @template T
+ * @template U
+ * @param {(IterableIterator<T> | AsyncIterableIterator<T>)} iter
+ * @param {(IterableIterator<U> | AsyncIterableIterator<U>)} sink
+ * @returns {Promise<void>}
+ */
 export async function send<T, U>(
     iter: IterableIterator<T> | AsyncIterableIterator<T>,
     sink: IterableIterator<U> | AsyncIterableIterator<U>,
@@ -130,6 +230,15 @@ export async function broadcast<T>(
     }
 }
 
+/**
+ * Create a consumer using fn by calling fn on the incoming value.
+ *
+ * @export
+ * @template T
+ * @template U
+ * @param {(...args: Array<T>) => any} fn
+ * @returns
+ */
 export function consumer<T, U>(fn: (...args: Array<T>) => any) {
     const consume = coroutine<T, U, AsyncIterableIterator<U>>(async function* (): AsyncIterableIterator<U> {
         while (true) {
