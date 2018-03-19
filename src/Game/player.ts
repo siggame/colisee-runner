6d15903cdd099b52e46427e08f71df4d3412191c
user404d <conduffql@gmail.com>
2018-03-18 21:21:53 -0500

    Fix issue with client timeout

--- src/Game/player.ts
+++ src/Game/player.ts
@@ -27,9 +27,6 @@ export function make_play_game(
         // pull client images
         await prepare_game_clients(docker, docker_options, game);
 
-        // wait to clean up after misbehaving clients
-        delayed_client_cleanup(docker, game, 30);
-
         game.status = "playing";
 
         // run client containers
@@ -92,28 +89,6 @@ function prepare_game_clients(
     ).catch((e) => { winston.error("Pull Failed"); throw e; });
 }
 
-function delayed_client_cleanup(docker: Docker, { submissions }: IGame, timeout: number) {
-    const game_client_names = submissions.map(({ id: sub_id, team: { id: team_id } }) =>
-        `/team_${team_id}_${sub_id}`,
-    );
-    setTimeout(() => {
-        docker.listContainers((error, containers) => {
-            if (containers) {
-                const game_clients = containers.filter((container) =>
-                    game_client_names.some((name) => container.Names.indexOf(name) >= 0),
-                );
-                if (game_clients.some((client) => client.Status !== "running")) {
-                    game_clients.forEach((client) => {
-                        docker.getContainer(client.Id).remove({ force: true }, (error, container) => {
-                            winston.info("stopping and removing container", container);
-                        });
-                    });
-                }
-            }
-        });
-    }, timeout * 1000);
-}
-
 function run_game_clients(
     docker: Docker,
     { hostname, game_port, network_name }: IGameServerOptions,
@@ -122,19 +97,29 @@ function run_game_clients(
     return Promise.all(
         submissions.map(async function ({ id: sub_id, image, log, version, team: { name, id: team_id } }) {
             if (log) {
-                const container: Docker.Container = await docker.run(`${image}`,
-                    ["-n", `${name}`, "-s", `${hostname}:${game_port}`, "-r", `${game_id}`],
-                    log, { name: `team_${team_id}_${sub_id}`, HostConfig: { NetworkMode: network_name } });
-                const info = await container.inspect();
-                const data: fs.ReadStream = await container.remove({ force: true });
-                if (data) {
-                    winston.info(`Removing ${image}`);
-                    data.on("data", (chunk) => {
-                        winston.info(chunk.toString("utf8"));
-                    });
-                }
-                if (info.State.ExitCode === 127) {
-                    throw new Error(`Entrypoint command failed for image ${image}.`);
+                const temp = new PassThrough();
+                temp.pipe(log, { end: false });
+                try {
+                    const on_time = await Promise.race([
+                        delay(15 * 60 * 1000).then(_ => false),
+                        docker.run(`${image}`,
+                            ["-n", `${name}`, "-s", `${hostname}:${game_port}`, "-r", `${game_id}`],
+                            temp, {
+                                HostConfig: { AutoRemove: true, NetworkMode: network_name },
+                                StopTimeout: 1,
+                                name: `team_${team_id}_${sub_id}`,
+                            },
+                        ).then(_ => true),
+                    ]);
+                    if (!on_time) {
+                        winston.error("client timeout reached");
+                        log.write("\n\ntimeout reached");
+                        docker.getContainer(`team_${team_id}_${sub_id}`).stop();
+                    }
+                } catch (error) {
+                    throw error;
+                } finally {
+                    log.end();
                 }
             }
         }),
